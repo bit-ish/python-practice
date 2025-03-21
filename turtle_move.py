b68489fb91b98b8f78ebbcdd2c2795c4924199c5
ishy <ishy23official@gmail.com>
2025-03-21 15:01:28 +0530

    made a spirograph using tutle

--- turtle_move.py
+++ turtle_move.py
@@ -1,29 +1,45 @@
+import turtle
 from turtle import Turtle,Screen
 import random
 
+turtle.colormode(255)
 my_screen=Screen()
-colors=["red", "blue", "green", "yellow", "purple", "orange", "pink", "black", "brown", "gray"]
+
+def random_color():
+    r=random.randint(0,255)
+    g=random.randint(0,255)
+    b=random.randint(0,255)
+
+    colors=(r,g,b)
+    return colors
 
 x=random.randint(0,4)
 
 buns=Turtle()
-buns.speed(0)
-buns.pensize(10)
+buns.speed("fastest")
+buns.pensize(2)
 buns.shape="turtle"
-buns.color("violet","yellow")
+# buns.color(random_color())
 
-for a in range(200):
-    x = random.randint(0, 4)
-    buns.color(random.choice(colors))
+# for a in range(100):
+#     x = random.randint(0, 4)
+#     buns.color(random_color())
+#
+#     buns.color()
+#     buns.fd(20)
+#     buns.setheading(90*x)
+#     print(90*x)
 
-    buns.color()
-    buns.fd(20)
-    buns.setheading(90*x)
-    print(90*x)
+# heading=0
+
+def spirograph(gap):
+    for b in range(int(360/gap)):
+        buns.color(random_color())
+        buns.seth(buns.heading()+gap)
+
+        buns.circle(100)
 
 
-
-
-
+spirograph(15)
 
 my_screen.exitonclick()
