e8b0ef6a3debd3f088770f927eb12ccbc5f05be3
ishy <ishy23official@gmail.com>
2025-03-22 13:29:15 +0530

    made etch a sketch

---
+++ PycharmProjects/day-19/etch_a_sketch.py
@@ -0,0 +1,38 @@
+from turtle import Turtle,Screen
+
+
+timmy=Turtle()
+my_screen=Screen()
+
+timmy.shape("turtle")
+
+def move_fd():
+    timmy.fd(20)
+def move_bk():
+    timmy.bk(20)
+def move_left():
+    timmy.left(10)
+def move_right():
+    timmy.right(10)
+
+def clr_screen():
+    my_screen.clearscreen()
+
+
+
+my_screen.onkey(move_fd,"w")
+my_screen.onkey(move_bk,"s")
+
+my_screen.onkey(move_left,"a")
+my_screen.onkey(move_right,"d")
+
+my_screen.onkey(clr_screen,"c")
+
+
+
+
+my_screen.listen()
+
+my_screen.exitonclick()
+
+
